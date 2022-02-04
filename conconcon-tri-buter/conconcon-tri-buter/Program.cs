c9f3016145bd718a96df68adee34d29de09b8f2e
Dynesshely <2369913161@qq.com>
2022-02-04 15:30:25 +0800

    haha

--- conconcon-tri-buter/conconcon-tri-buter/Program.cs
+++ conconcon-tri-buter/conconcon-tri-buter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace conconcon_tri_buter
 {
@@ -9,9 +10,97 @@ namespace conconcon_tri_buter
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine($"{Environment.CurrentDirectory}");
+            Console.Write("How much days you want to contribute? : ");
+            int days = int.Parse(Console.ReadLine());
+            Console.Write("How many contribution one day pushed ('r' - random) : ");
+            string rst = Console.ReadLine();
+            bool random = rst == "r";
+            int cons = random ? -1 : int.Parse(rst);
 
-            Process process = new();
+            Console.WriteLine("\nPress any key to continue ...\n");
+            Console.ReadLine(); Console.WriteLine();
 
+            DateTime dt = DateTime.Now, now = DateTime.Now;
+            dt -= new TimeSpan(days, 0, 0, 0);
+            while(dt.Year != now.Year || dt.Month != now.Month || dt.Day != now.Day)
+            {
+                Console.WriteLine($"date: {dt:yyyy-MM-dd} | start commit!");
+                for (int i = 1; i <= (random ? rand.Next(5, 9) : cons); ++ i)
+                    generatefile(dt);
+                dt = dt.AddDays(1);
+            }
+
+            Console.WriteLine("\n\nFinished! Go to your GitHub to see 13 !");
+        }
+
+        private static void generatefile(DateTime dt)
+        {
+            string fn = $"{Environment.CurrentDirectory}\\{randomname()}.txt";
+
+            Console.WriteLine($"generate: {Path.GetFileName(fn)}");
+
+            if (File.Exists(fn))
+                File.Delete(fn);
+            FileStream fs = File.Create(fn);
+            StreamWriter sw = new(fs);
+            sw.WriteLine(randomname());
+            sw.Flush();
+            sw.Close(); fs.Close();
+
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "git.exe";
+                myProcess.StartInfo.Arguments = " add .";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                myProcess.WaitForExit();
+            }
+
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "git.exe";
+                myProcess.StartInfo.Arguments = $" commit -m \"{randomname()}\" --date {dt:yyyy/MM/dd}";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                myProcess.WaitForExit();
+            }
+
+            File.Delete(fn);
+
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "git.exe";
+                myProcess.StartInfo.Arguments = " add .";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                myProcess.WaitForExit();
+            }
+
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "git.exe";
+                myProcess.StartInfo.Arguments = $" commit -m \"{randomname()}\" --date {dt:yyyy/MM/dd}";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                myProcess.WaitForExit();
+            }
+
+            Console.WriteLine($"delete: {Path.GetFileName(fn)}");
+        }
+
+        static Random rand = new();
+
+        private static string randomname()
+        {
+            int length = 8;
+            string rst = "";
+            for (int i = 1; i <= length; ++ i)
+                rst += rand.Next(0, 1) == 0 ? ((char)('A' + rand.Next(0, 25))).ToString() : ((char)('a' + rand.Next(0, 25))).ToString();
+            return rst;
         }
     }
 }
