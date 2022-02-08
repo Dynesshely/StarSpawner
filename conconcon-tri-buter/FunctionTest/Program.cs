a504eae419dab7bbe60b681f0d9187ddd8163448
Dynesshely <2369913161@qq.com>
2022-02-09 04:14:44 +0800

    feat: FunctionTest project

---
+++ conconcon-tri-buter/FunctionTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+    }
+}
--- conconcon-tri-buter/cctb-wpf/contri-graph.xaml.cs
+++ conconcon-tri-buter/cctb-wpf/contri-graph.xaml.cs
@@ -20,9 +20,49 @@ namespace cctb_wpf
     /// </summary>
     public partial class contri_graph : UserControl
     {
+
         public contri_graph()
         {
             InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            Graph_Init();
+            Source_Init();
+        }
+
+        private Dictionary<DateTime, Rectangle> source = new();
+
+        public void Graph_Init()
+        {
+            graph.Children.Clear();
+            source.Clear();
+        }
+
+        public void Source_Init()
+        {
+            // 按总天数生成
+            //int days = getDays(DateTime.Now.Year);
+            //for(int i = 1; i <= days; ++ i)
+            //{
+            //    source.Add(DateTime.Now.AddDays(1 - i), new Rectangle()
+            //    {
+            //        Width = 10, Height = 10, RadiusX = 5, RadiusY = 5
+            //    });
+            //}
+
+            // 按标尺生成 (日标尺: 日/二/四/六)
+            int today = DateTime.Now.DayOfWeek
+        }
+
+        private int getDays(int year)
+        {
+            bool run = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ||
+                (year % 3200 == 0 && year % 172800 == 0);
+            return run ? 366 : 365;
         }
     }
 }
