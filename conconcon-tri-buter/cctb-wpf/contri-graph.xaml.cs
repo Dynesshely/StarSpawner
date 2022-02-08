df8a49458107708ec28f7c30da286bcfe8b6509c
Dynesshely <2369913161@qq.com>
2022-02-09 03:46:49 +0800

    feat(contri-graph): added contri-graph.xaml/.xaml.cs

---
+++ conconcon-tri-buter/cctb-wpf/contri-graph.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cctb_wpf
+{
+    /// <summary>
+    /// contri_graph.xaml 的交互逻辑
+    /// </summary>
+    public partial class contri_graph : UserControl
+    {
+        public contri_graph()
+        {
+            InitializeComponent();
+        }
+    }
+}
